dc5ee70a0f0b55e91a2547511e5a382900c02b17
Hamid <sir.lovelorne@gmail.com>
2022-11-05 16:43:33 +0330

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div className="bg-yellow-500 h-screen w-full flex justify-center">
+      <h1>HI</h1>
+    </div>
+  )
+}
+
+export default App
