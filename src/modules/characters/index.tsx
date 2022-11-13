d55c2b2aa3b3c6f69425f1d2efc2f402eca7ff1f
Hamid <sir.lovelorne@gmail.com>
2022-11-13 20:25:40 +0330

    Fix: import problem

--- src/modules/characters/index.tsx
+++ src/modules/characters/index.tsx
@@ -1,32 +1,43 @@
-import Photo from '../common/photo'
-import { CharactersProps } from './types'
+import Photo from '@/modules/common/Photo'
+import {CharactersProps} from './types'
 
-const CharactersContainer: React.FC<CharactersProps> = ({ id,
-    name,
-    image,
-    status,
-    species,
-    origin,
-    location }) => (
-
-
-    <div key={id} className="flex flex-row justify-center items-center border-b-2 p-5 space-y-5">
-        <div className='flex w-1/3 lg:text-2xl md:text-base font-bold truncate justify-center items-center text-center'>
-            <a href={`characters/${id}`}>
-                {name}
-                <Photo imageAddress={image} altName={name} />
-            </a>
-        </div>
-        <div className='flex flex-col w-2/3 p-5 leading-10 truncate text-gray-50'>
-
-            <p><span className='font-semibold  text-gray-400'>Status: </span>{status}</p>
-            <p><span className='font-semibold  text-gray-400'>Species:</span> {species}</p>
-            <p><span className='font-semibold  text-gray-400'>Origin Name:</span> {origin.name}</p>
-            <p><span className='font-semibold  text-gray-400'>Location Name:</span> {location.name}</p>
-
-        </div>
+const CharactersContainer: React.FC<CharactersProps> = ({
+  id,
+  name,
+  image,
+  status,
+  species,
+  origin,
+  location,
+}) => (
+  <div
+    key={id}
+    className='flex flex-row justify-center items-center border-b-2 p-5 space-y-5'
+  >
+    <div className='flex w-1/3 lg:text-2xl md:text-base font-bold truncate justify-center items-center text-center'>
+      <a href={`characters/${id}`}>
+        {name}
+        <Photo imageAddress={image} altName={name} />
+      </a>
     </div>
-
+    <div className='flex flex-col w-2/3 p-5 leading-10 truncate text-gray-50'>
+      <p>
+        <span className='font-semibold  text-gray-400'>Status: </span>
+        {status}
+      </p>
+      <p>
+        <span className='font-semibold  text-gray-400'>Species:</span> {species}
+      </p>
+      <p>
+        <span className='font-semibold  text-gray-400'>Origin Name:</span>{' '}
+        {origin.name}
+      </p>
+      <p>
+        <span className='font-semibold  text-gray-400'>Location Name:</span>{' '}
+        {location.name}
+      </p>
+    </div>
+  </div>
 )
 
 export default CharactersContainer
